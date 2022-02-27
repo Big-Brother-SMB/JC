dd2f7ef05f7f4e55da48637cacdc7971cfe0bbca
Azerthias <matthias.hayart@gmail.com>
2022-02-27 16:00:53 +0100

    Update crenau.js

--- public/crenau/crenau.js
+++ public/crenau/crenau.js
@@ -223,6 +223,35 @@ function algo(){
         let nbEmail = 0
         let fini = false
 
+        function sendEmail(prenom,email){
+            Email.send({
+                Host: "smtp.gmail.com",
+                Username: "foyer.beaucamps@gmail.com",
+                Password: "beaucamps",
+                To: email,
+                From: "foyer.beaucamps@gmail.com",
+                Subject: "Inscription validée",
+                Body: "Bonjour " + prenom + ", ton inscription au foyer le " + day[j] + " à " + (h+11) + "h a été validée",
+            })
+            .then(function (message) {
+                console.log(message)
+                if(message != "OK"){
+                    sendEmail(prenom,email)
+                }else{
+                    console.log("mail sent successfully to " + email)
+                    
+                    nbEmail++
+                    if(fini){
+                        document.getElementById("start algo").innerHTML = "fini, " + (inscrits - dejaInscrit) + " inscriptions<br>il reste " + (places - inscrits) + " places<br>appuyer pour reload<br>Email : " + nbEmail + "/" + (inscrits - dejaInscrit)
+                        if(nbEmail == (inscrits - dejaInscrit)){
+                            document.getElementById("start algo").innerHTML = "fini, " + (inscrits - dejaInscrit) + " inscriptions<br>il reste " + (places - inscrits) + " places<br>appuyer pour reload<br>Email : fini"
+                        }
+                    }
+                }
+                
+            });
+        }
+
         while(places > inscrits){
             console.log("inscrit",inscrits)
             if(!tag[alea] && addLinkTag[alea].length <= places - inscrits){
@@ -241,25 +270,7 @@ function algo(){
                                 let email = snapshot.val()
                                 let prenom = users[p].split(" ")[0]
                                 console.log(email)
-                            Email.send({
-                                Host: "smtp.gmail.com",
-                                Username: "foyer.beaucamps@gmail.com",
-                                Password: "beaucamps",
-                                To: email,
-                                From: "foyer.beaucamps@gmail.com",
-                                Subject: "Inscription validée",
-                                Body: "Bonjour " + prenom + ", ton inscription au foyer le " + day[j] + " à " + (h+11) + "h a été validée",
-                            })
-                                .then(function (message) {
-                                console.log("mail sent successfully to " + email,message)
-                                nbEmail++
-                                if(fini){
-                                    document.getElementById("start algo").innerHTML = "fini, " + (inscrits - dejaInscrit) + " inscriptions<br>il reste " + (places - inscrits) + " places<br>appuyer pour reload<br>Email : " + nbEmail + "/" + (inscrits - dejaInscrit)
-                                    if(nbEmail == (inscrits - dejaInscrit)){
-                                        document.getElementById("start algo").innerHTML = "fini, " + (inscrits - dejaInscrit) + " inscriptions<br>il reste " + (places - inscrits) + " places<br>appuyer pour reload<br>Email : fini"
-                                    }
-                                }
-                                });
+                                sendEmail(prenom,email)
                             })
                             
                         }catch(exception){
